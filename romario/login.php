65b64ee7bc164a1001c289656b93952db54daa0e
Romario114 <romario1479@edu.uniube.br>
2025-09-18 20:05:27 -0300

    Update login.php

--- romario/login.php
+++ romario/login.php
@@ -2,20 +2,32 @@
 include ("conexao.php");
 
 $cpf = $_POST["cpf"];
-$senha=$_POST["senha"];
+$senha = $_POST["senha"];
 
-$sql = "select nome from usuarios where cpf='$cpf' and senha='$senha'";
+$sql = "SELECT nome FROM usuarios WHERE cpf = ? AND senha = ?";
+$stmt = $conn->prepare($sql);
 
-$resultado = $conn->query($sql);
-$row = $resultado->fetch_assoc();
+if ($stmt) {
+    $stmt->bind_param("ss", $cpf, $senha);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-if (isset($row) && $row['nome']!=''){
-    session_start();
-    $_SESSION["cpf"] = $cpf;
-    $_SESSION["senha"] = $senha;
-    $_SESSION["nome"] = $row['nome'];
-    header("Location: principal.php");
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if (isset($row) && $row['nome'] != '') {
+            session_start();
+            $_SESSION["cpf"] = $cpf;
+            $_SESSION["senha"] = $senha;
+            $_SESSION["nome"] = $row['nome'];
+            header("Location: principal.php");
+            exit;
+        } else {
+            echo "Senha incorreta!";
+        }
+    } else {
+        echo "Erro de conexão no banco.";
+    }
+} else {
+    echo "Erro na SQL.";
 }
-
-
 ?>
